1eb22006ea090fe915c45211285ce62eba83bf8c
Moon Song it <67774744+MoonSongIT@users.noreply.github.com>
2024-07-19 11:06:39 +0900

    js-arrowUp

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
+
 const header = document.querySelector('.header');
+const home = document.querySelector('#home');
+const arrowUp = document.querySelector('.arrow-up');
+
 const headerHeight = header.getBoundingClientRect().height;
-// console.log(headerHeight);
+const homeHeight = home.getBoundingClientRect().height;
+
 document.addEventListener('scroll', ()=> {
-    console.log(window.scrollY);
+    scrollEvent();
+});
+
+function scrollEvent (){
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
+        home.style.opacity = 0;
+
     } else {
         header.classList.remove('header--dark');
+        home.style.opacity = 1 - (window.scrollY / headerHeight);
     }
-});
+   // console.log(`window.scroll:${window.scrollY}  homeHeight / 2: ${ homeHeight / 2} `)
+    if (window.scrollY < homeHeight / 2){
+        arrowUp.style.opacity = 0;
+    }
+    else {
+        arrowUp.style.opacity = 1;
+    }
+
+
+}
